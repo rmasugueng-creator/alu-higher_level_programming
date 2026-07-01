d6716371c18cf2fd15b178fe2c1e1933e4e489b0
rmasugueng-creator <r.masugueng@alustudent.com>
2026-07-01 07:49:49 +0000

    Task 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, object-oriented programming language that combines remarkable power with very clear syntax"
-str = str[str.find("obj"):str.find(" lan")] + str[str.find(" with"):str.find(" very")] + " " + str[:6]
+str = ("Python is an interpreted, object-oriented programming language "
+       "that combines remarkable power with very clear syntax")
+str = (str[str.find("obj"):str.find(" lan")] +
+       str[str.find(" with"):str.find(" very")] + " " + str[:6])
 print(str)
-
