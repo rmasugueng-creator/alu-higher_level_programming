ef06234d0e21eee407d405dfdb77cbe377ad4eec
rmasugueng-creator <r.masugueng@alustudent.com>
2026-07-26 12:30:40 +0000

    task 4

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -89,4 +89,3 @@ class Rectangle:
     def __repr__(self):
         """Return a string representation to recreate the rectangle."""
         return "Rectangle({}, {})".format(self.__width, self.__height)
-    
